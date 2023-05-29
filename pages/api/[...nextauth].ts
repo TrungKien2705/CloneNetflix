e8543fe59b355a56c77a8aec51dceb2295d9db06
TrungKien2705 <nt.kien.2705@gmail.com>
2023-05-30 00:13:25 +0700

    first commit

---
+++ pages/api/[...nextauth].ts
@@ -0,0 +1,6 @@
+import NextAuth from "next-auth/next";
+import Credentials from "next-auth/providers/credentials";
+
+// export default NextAuth{
+
+// }
---
+++ pages/test.tsx
@@ -0,0 +1,5 @@
+const Test = () => {
+  return <h1>Hello Word</h1>;
+};
+
+export default Test;
